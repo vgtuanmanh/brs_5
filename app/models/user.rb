23203042b2086b3f742ccc4870bbb42e085b8f5a
vgtuanmanh <vgtuanmanh@gmail.com>
2015-03-24 15:41:50 +0700

    Add role to users, namespace for admin, admin manage user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
+  before_save {self.email = email.downcase}
+
+  ROLES = %w{admin user}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :role , inclusion: ROLES
 end
